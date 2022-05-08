7533453557488a012c6c8e37f9305702f0130ac4
camilevahviraki <camilevahviraki@gmail.com>
2022-05-08 21:37:08 +0200

    Add link in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SingIn from './modules/SingIn';
+import SignUp from './modules/signUp';
+import SimpleMap from './modules/google-map-direction';
+import MyComponent from './modules/maps-api/google-map-api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+     <Route path="/" element={<App />}/>
+     <Route path='/SignUp' element={<SignUp/>}/>
+     <Route path='/SignIn' element={<SingIn/>}/>
+     <Route path='/mapGoogle' element={<SimpleMap/>}/>
+     <Route path='/GoogleMapApi' element={<MyComponent/>}/>
+  </Routes>
+  
+    
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
