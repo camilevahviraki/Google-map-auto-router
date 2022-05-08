520d3250e9a2ddaa9650c13dafd6b9239e9cfed5
camilevahviraki <camilevahviraki@gmail.com>
2022-05-08 21:36:28 +0200

    Update signIn.js file

--- src/modules/SingIn.js
+++ src/modules/SingIn.js
@@ -1,20 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import {appFirebas, analytics} from './firebaseAuth';
+import validator from 'validator';
+import Axios from 'axios';
 
 
 function SingIn() {
 
     
     const [mailAuth, setMailAuth] = useState('');
-    const [pass, setPass] = useState('');
+    const [passAuth, setPass] = useState('');
+    const [connection, setConnection] = useState('');
+    const [formMsg, setFormMSg] = useState('');
 
     const handleSignIn = (e) => {
-        alert('Signed In')
+        if(mailAuth.length === 0 ||passAuth.length === 0) {
+            setFormMSg('Please, fill all inpusts!')
+        }else if(!validator.isEmail(mailAuth)){
+            setFormMSg('Enter a valid email')
+        }else {
+          setFormMSg('welcom to vitron')
+          Axios.post('http://localhost:8000/signIn',
+          {
+            mailAuth: mailAuth,
+            passwordAuth: passAuth,
+          }).then((response, error) => {
+            if(error)console.log(error)
+            if (response)console.log(response)
+          }) ;
+        }
     }
 
   return (
     <div>
-        <form onSubmit={handleSignIn}>
+       
             <input type="mail"
             className="mailAuth" 
             placeholder="enter your mail"
@@ -25,8 +43,13 @@ function SingIn() {
             placeholder="enter your password"
             onChange={(e) => setPass(e.target.value)}
             />
-            <button type='submit' >Sign In</button>
-        </form>
+            <p>Kip me signed in</p>
+            <input type='checkbox' 
+            onChange={(e) => setConnection(e.target.value)}
+            />
+            <button onClick={handleSignIn} >Sign In</button>
+            <p>{formMsg}</p>
+       
     </div>
   )
 }
