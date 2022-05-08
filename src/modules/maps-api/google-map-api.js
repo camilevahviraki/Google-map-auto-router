a35ff41dc3b396a0f34ad8e53ffe9fb4ba3cb117
camilevahviraki <camilevahviraki@gmail.com>
2022-05-08 21:40:49 +0200

    Add map-api folder

---
+++ src/modules/maps-api/google-map-api.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '500px'
+};
+
+const center = {
+  lat: -1.935114,
+  lng: 30.082111
+};
+
+// const typeMap = {
+//   MapTypeId:SATELLITE
+// }
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyCv3MXffg1JoxNfP7xPDWkBbib3aA2tC2o"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={8}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        // mapTypeId={"ROADMAP"}
+        // mapTypeId={maps.MapTypeId.SATELLITE}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default MyComponent
